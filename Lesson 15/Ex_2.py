cfe2eb4b08fa7ff7463547f9b239cc59f68b71ff
ionmilescu <120732998+ionmilescu@users.noreply.github.com>
2023-01-16 18:37:54 +0200

    ex 1 & 2

--- Lesson 15/Ex_2.py
+++ Lesson 15/Ex_2.py
@@ -1,24 +1,6 @@
 import os
 
 
-
-
-
-folder_path = input("Enter the folder path: ")
-
-if os.path.exists(folder_path):
-    if os.path.isdir(folder_path):
-        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-        for file in files:
-            print(file)
-    else:
-        print("Provided path is not a folder.")
-else:
-    print("Provided path does not exist.")
-    
-
-
-
 def list_files_in_folder(folder_path):
     if os.path.exists(folder_path):
         if os.path.isdir(folder_path):
@@ -29,3 +11,13 @@ def list_files_in_folder(folder_path):
             print("Provided path is not a folder.")
     else:
         print("Provided path does not exist.")
+        
+
+
+def rename_file(src, dst):
+    os.rename(src, dst)
+    print(f"{src} has been renamed to {dst}")
+
+src_path = os.path.join(list_files_in_folder)
+dst_path = os.path.join(list_files_in_folder)
+rename_file(src_path, dst_path)
